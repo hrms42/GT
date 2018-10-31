1c041aeba76d21a45f756e6f16d0f91a7fa74876
hrms42 <s1230242@u-aizu.ac.jp>
2018-10-31 17:47:59 +0900

    Oct/31

--- src/view.cpp
+++ src/view.cpp
@@ -20,7 +20,32 @@ void signed_distance_sphere(Eigen::MatrixXd& GV, Eigen::VectorXd& S)
     double x = GV(i,0);
     double y = GV(i,1);
     double z = GV(i,2);
-    S(i) = 1.0 - std::sqrt(x*x + y*y + z*z);
+    S(i) = 0.1 - std::sqrt(x*x + y*y + z*z);
+  }
+}
+
+//look carefully textbook and PDF
+void grid_spheres(Eigen::MatrixXd& GV, Eigen::VectorXd& S)
+{
+    int rows = GV.rows();
+    S.resize(rows);
+    for (int i = 0; i < rows; ++i) {
+      double x = GV(i,0);
+      double y = GV(i,1);
+      double z = GV(i,2);
+      //center of sphere 
+      double xc = V(0,0);
+      double yc = V(0,1);
+      double zc = V(0,2);
+      
+      S(i) = 0.005 - std::sqrt((x-xc)*(x-xc) + (y-yc)*(y-yc) + (z-zc)*(z-zc));
+      for(int j = 1 ; j < V.rows() ; j++ ){
+	      xc = V(j,0);
+	      yc = V(j,1);
+	      zc = V(j,2);
+      
+	      S(i) = std::max( S(i),  0.005 - std::sqrt((x-xc)*(x-xc) + (y-yc)*(y-yc) + (z-zc)*(z-zc)));
+      }
   }
 }
 
@@ -35,18 +60,9 @@ int main( int argc, char *argv[] )
   //const Eigen::RowVector3i res(65,65,65);
   //Eigen::MatrixXd GV(res(0)*res(1)*res(2),3);
 
-  x_max = y_max = z_max = x_min = y_min = z_min = 0.0;
+  x_max = y_max = z_max = -99999.0;
+  x_min = y_min = z_min =  99999.0;
   
-  //Eigen::VectorXd S;
-  //  S.resize(GV.rows());
-  //p[3]=0.2*igl::avg_edge_length(V,F);
-  //for(int i = 0 ; i < GV.rows() ; i++ ){
-  //   p[0]=V(i,0);
-  //   p[1]=V(i,1);
-  //   p[2]=V(i,2);
-  //	 S(i)=sphere(0.0,0.0,0.0,p);
-  //}
-
   for(int i = 0 ; i < V.rows() ; i++ ){
      if( x_max < V(i,0) ) x_max = V(i,0);
 	 if( y_max < V(i,1) ) y_max = V(i,1);
@@ -56,20 +72,39 @@ int main( int argc, char *argv[] )
 	 if( z_min > V(i,2) ) z_min = V(i,2);
   }
 
+  std::cout<<x_max<<", "<<y_max<<", "<<z_max<<std::endl;
+  std::cout<<x_min<<", "<<y_min<<", "<<z_min<<std::endl;
+  //return 0;
+
+  //calcurate a diagonal
   const double min = -0.2*sqrt(std::pow(x_max-x_min,2.0)+std::pow(y_max-y_min,2.0)+std::pow(z_max-z_min,2.0));
   const double max =  0.2*sqrt(std::pow(x_max-x_min,2.0)+std::pow(y_max-y_min,2.0)+std::pow(z_max-z_min,2.0));
 
-  std::cout<<min<<std::endl;
-  const Eigen::RowVector3d Vmin(min,min,min);
-  const Eigen::RowVector3d Vmax(max,max,max);
+
+  //enlarged
+  //std::cout<<min<<std::endl;
+  const Eigen::RowVector3d Vmin(x_min+min,y_min+min,z_min+min);
+  const Eigen::RowVector3d Vmax(x_max+max,y_max+max,z_max+max);
 
   const double h = (Vmax-Vmin).maxCoeff()/(double)s;
   const Eigen::RowVector3i res = (s*((Vmax-Vmin)/(Vmax-Vmin).maxCoeff())).cast<int>();
   
   Eigen::MatrixXd GV(res(0)*res(1)*res(2),3);
-   
+ 
+  //Eigen::VectorXd S;
+  //S.resize(GV.rows());
+  //p[3]=0.2*igl::avg_edge_length(V,F);
+  //for(int i = 0 ; i < GV.rows() ; i++ ){
+  //   p[0]=GV(i,0);
+  //   p[1]=GV(i,1);
+  //   p[2]=GV(i,2);
+  //   S(i)=sphere(2.0,2.0,2.0,p);
+  //}
+
+  //calculate grid subdivision by x/128 
   for(int zi = 0;zi<res(2);zi++)
   {
+    //cartesian look test p.17
     const auto lerp = [&](const int di, const int d)->double
       {return Vmin(d)+(double)di/(double)(res(d)-1)*(Vmax(d)-Vmin(d));};
     const double z = lerp(zi,2);
@@ -84,9 +119,10 @@ int main( int argc, char *argv[] )
     }
   }
 
+  
   Eigen::VectorXd S;
-
-  signed_distance_sphere(GV,S);
+  S.resize(GV.rows());
+  grid_spheres(GV,S);
  
   Eigen::MatrixXd SV;
   Eigen::MatrixXi SF; 
